84919f7fcdec8413f9c54ff1f5a947049c665ec8
Parthiban-git <76425798+Parthiban-git@users.noreply.github.com>
2021-01-18 16:58:28 +0530

    cors

--- server_side/index.js
+++ server_side/index.js
@@ -1,14 +1,17 @@
 const express=require('express')
+const cors=require('cors')
+
 
 const app=express();
+app.use(cors())
 
 app.post('/hai',(req,res)=>{
+    console.log(req.body)
     if(req.body==='hai'){
         res.send('hello')
     }
 })
+
 app.listen(3000,()=>{
     console.log('port running successfully')
 })
-
-
