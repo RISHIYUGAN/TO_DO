c383168b310990bfffff81d84be5b18e78273c4f
Parthiban-git <76425798+Parthiban-git@users.noreply.github.com>
2021-01-19 09:07:50 +0530

    corss

--- server_side/src/index.js
+++ server_side/src/index.js
@@ -1,8 +1,10 @@
 const express=require('express')
+const cors=require('cors')
 const app=express()
 const User=require('./models/user')
 const port=process.env.PORT || 3000
 app.use(express.json())
+app.use(cors())
 require('../Database/mongoose')
 
 app.post('/todo-list',(req,res)=>{
